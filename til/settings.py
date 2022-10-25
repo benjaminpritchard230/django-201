5ebf42cd125f8f1e97e04f32a21f047e684643dc
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2022-10-25 01:32:43 +0100

    Learning django

--- til/settings.py
+++ til/settings.py
@@ -153,3 +153,9 @@ AUTHENTICATION_BACKENDS = (
 )
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_DIR, "frontend/")
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = "/static/"
